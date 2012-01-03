66c77c75fbc56fedd6faefeeae2852f2d5ebb396
Chris Hansen <yodarunamok@gmail.com>
2012-01-03 11:58:50 -0700

    Minor tweaks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,7 @@
-# Set up the relevant port for the Riak client (for whatever reason, it doesn't yet use ripple.yml)
-if Rails.env.production?
-  RIAK_HTTP_PORT = 8098
-elsif Rails.env.test?
-  RIAK_HTTP_PORT = 9100
-else
-  RIAK_HTTP_PORT = 9000
-end
-
 class IsUniqueValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value.length > 0
-      client = Riak::Client.new :solr => '/solr', :http_port => RIAK_HTTP_PORT
-      result = client.search('users', "#{attribute.to_s}:#{value.downcase}")
+      result = Ripple::client.search('users', "#{attribute.to_s}:#{value.downcase}")
       record.errors.add attribute, "must be unique" unless result['response']['numFound'] == 0
     end
   end
@@ -27,7 +17,7 @@ class User
   property 'email', :String, :presence => true, :format => { :with => email_regex }, :is_unique => true
 
   def initialize(*args)
-    client = Riak::Client.new :solr => '/solr', :http_port => RIAK_HTTP_PORT
+    client = Ripple::client
     client['users'].enable_index! unless client['users'].is_indexed?
     super
   end
