96f67700c0d9cbb0ae36060e231627b02974fb14
Chris Hansen <yodarunamok@gmail.com>
2012-02-07 17:10:42 -0700

    Chapter 10 temporary.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+
   def new
     @user = User.new
     @title = 'Sign up'
@@ -20,4 +22,26 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = @user.name
   end
+
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
 end
